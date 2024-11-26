32a303fc7d838e2b9d51d343d36ec0722eb9a6b7
Emre Caylar <emre.caylar@student.kdg.be>
2024-11-26 13:06:54 +0100

    get all invites for player

--- platform/src/main/java/be/kdg/integration5/platform/adapters/out/db/repositories/InviteRepository.java
+++ platform/src/main/java/be/kdg/integration5/platform/adapters/out/db/repositories/InviteRepository.java
@@ -2,9 +2,13 @@ package be.kdg.integration5.platform.adapters.out.db.repositories;
 
 import be.kdg.integration5.platform.adapters.out.db.entities.InviteJpaEntity;
 import be.kdg.integration5.platform.adapters.out.db.entities.LobbyJpaEntity;
+import be.kdg.integration5.platform.adapters.out.db.entities.PlayerJpaEntity;
+import be.kdg.integration5.platform.domain.Invite;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface InviteRepository extends JpaRepository<InviteJpaEntity, UUID> {
+    List<InviteJpaEntity> getInvitesByRecipient(PlayerJpaEntity recipient);
 }
