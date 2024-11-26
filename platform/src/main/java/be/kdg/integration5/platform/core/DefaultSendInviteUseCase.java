d71c77b829fb43fda05199880feea3ecec01f955
Emre Caylar <emre.caylar@student.kdg.be>
2024-11-26 13:10:26 +0100

    added exception so user cant invite himeself

--- platform/src/main/java/be/kdg/integration5/platform/core/DefaultSendInviteUseCase.java
+++ platform/src/main/java/be/kdg/integration5/platform/core/DefaultSendInviteUseCase.java
@@ -3,6 +3,7 @@ package be.kdg.integration5.platform.core;
 import be.kdg.integration5.platform.domain.Invite;
 import be.kdg.integration5.platform.domain.Lobby;
 import be.kdg.integration5.platform.domain.Player;
+import be.kdg.integration5.platform.exceptions.InvalidInviteUserException;
 import be.kdg.integration5.platform.exceptions.InvalidLobbyException;
 import be.kdg.integration5.platform.exceptions.InvalidPlayerException;
 import be.kdg.integration5.platform.ports.in.GetLobbyUseCase;
@@ -10,6 +11,8 @@ import be.kdg.integration5.platform.ports.in.GetPlayerUseCase;
 import be.kdg.integration5.platform.ports.in.PlayerCreatesInviteUseCase;
 import be.kdg.integration5.platform.ports.out.InviteCreatePort;
 import be.kdg.integration5.platform.ports.out.LobbyLoadPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -34,10 +37,14 @@ public class DefaultSendInviteUseCase implements PlayerCreatesInviteUseCase {
         if (senderPlayer.isEmpty()) {
             throw new InvalidPlayerException("Sender not found");
         }
+
         Lobby lobby = lobbyLoadPort.loadLobby(lobbyId).orElseThrow(() -> new InvalidLobbyException("Lobby not found"));
         Optional<Player> recipientPlayer = getPlayerUseCase.getPlayerById(recipient);
         if (recipientPlayer.isEmpty()) {
             throw new InvalidPlayerException("Recipient not found");
+        }
+        if (senderPlayer == recipientPlayer) {
+            throw new InvalidInviteUserException("Sender and recipient are the same");
         }
         Invite invite = new Invite(UUID.randomUUID(), senderPlayer.get(), recipientPlayer.get(), lobby);
         inviteCreatePort.createInvite(invite);
