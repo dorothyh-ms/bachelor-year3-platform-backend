ab3849e40b14cfeb9ebd24f019b5c87785f85d70
IWantToCry <emre.ccaylar@hotmail.com>
2024-12-15 15:59:16 +0100

    updated prompt, added question refusal, rule files are now json instead of txt

--- application/src/main/resources/privileges.sql
+++ application/src/main/resources/privileges.sql
@@ -2,3 +2,8 @@ CREATE DATABASE platform;
 GRANT ALL ON platform.* TO 'user'@'%';
 GRANT SHOW DATABASES ON *.* TO 'user'@'%';
 FLUSH PRIVILEGES;
+
+CREATE DATABASE statistics;
+GRANT ALL ON statistics.* TO 'user'@'%';
+GRANT SHOW DATABASES ON *.* TO 'user'@'%';
+FLUSH PRIVILEGES;
